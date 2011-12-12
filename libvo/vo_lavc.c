4019e63bea71216683ee4d24f6a406cd5f5d21f3
Rudolf Polzer <divverent@xonotic.org>
2011-12-12 15:43:39 +0100

    fix a parameter mismatch

--- libvo/vo_lavc.c
+++ libvo/vo_lavc.c
@@ -90,7 +90,7 @@ static void uninit(struct vo *vo)
 
 static int config(struct vo *vo, uint32_t width, uint32_t height,
                   uint32_t d_width, uint32_t d_height, uint32_t flags,
-                  char *title, uint32_t format)
+                  uint32_t format)
 {
     struct priv *vc = vo->priv;
     enum PixelFormat pix_fmt = imgfmt2pixfmt(format);
