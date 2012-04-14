7f0795056b88257d51d53c1d99c164967b5bef7a
wm4 <wm4@mplayer2.org>
2012-04-14 13:39:53 +0200

    win32: move global variables into a struct & some refactoring
    This reflects the changes done to x11_common in mplayer2 some years
    ago. It makes it possible to open multiple VOs at once.
    
    The removed defines are probably for ancient versions of MinGW with
    incomplete headers.
    
    Remove some minor code duplication.

--- libvo/video_out.h
+++ libvo/video_out.h
@@ -260,6 +260,7 @@ struct vo {
     void *priv;
     struct MPOpts *opts;
     struct vo_x11_state *x11;
+    struct vo_w32_state *w32;
     struct vo_cocoa_state *cocoa;
     struct mp_fifo *key_fifo;
     struct input_ctx *input_ctx;
