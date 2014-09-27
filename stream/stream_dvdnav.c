a7b84fc12034febbd241b719c43772adc5a05492
Oleg Oshmyan <chortos@inbox.lv>
2014-09-27 15:23:09 +0200

    Fix building with libdvdnav 4.2.1

--- stream/stream_dvdnav.c
+++ stream/stream_dvdnav.c
@@ -715,13 +715,13 @@ static int mp_dvdnav_get_aid_from_format (stream_t *stream, int index, uint8_t l
 
   format = dvdnav_audio_stream_format(priv->dvdnav, lg);
   switch(format) {
-  case DVDNAV_FORMAT_AC3:
+  case DVD_AUDIO_FORMAT_AC3:
     return index + 128;
-  case DVDNAV_FORMAT_DTS:
+  case DVD_AUDIO_FORMAT_DTS:
     return index + 136;
-  case DVDNAV_FORMAT_LPCM:
+  case DVD_AUDIO_FORMAT_LPCM:
     return index + 160;
-  case DVDNAV_FORMAT_MPEGAUDIO:
+  case DVD_AUDIO_FORMAT_MPEG2_EXT:
     return index;
   default:
     return -1;
